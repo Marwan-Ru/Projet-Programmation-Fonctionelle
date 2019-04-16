83be80392c348b8cb286295ba86ef40050e1e876
Doo5942 <doodescamps@gmail.com>
2019-04-16 21:59:27 +0200

    tentative de test, la fonction ne fonctionne pas

--- list_tri.ml
+++ list_tri.ml
@@ -13,12 +13,18 @@ let partitionne_pivot_bis croissant l pivot l1 l2 =
 match l with
 []-> (l1, l2)
 |x::r -> if (croissant x pivot)
-         then (x::l1,l2)
-         else (l1, x::l2) ;;
+         then partitionne_pivot_bis croissant r pivot (x::l1) l2
+         else partitionne_pivot_bis croissant r pivot l1 (x::l2) ;;
 
-let partionne_pivot croissant l pivot =
-partitionne_pivot_bis croissant l pivot [] [] ;;
+let partitionne_pivot croissant l  =
+partitionne_pivot_bis croissant l (List.hd l) [] [] ;;
 
+let list = random_list 100 20 ;;
+partitionne_pivot croissant list ;;
+
+(*
+partitionne_pivot croissant
+*)
 (* 2. *) 
 
 
