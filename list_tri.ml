011b297bb8b09f339f64cae4187fe979bbd4dbfe
Doo5942 <doodescamps@gmail.com>
2019-04-16 13:45:00 +0200

    tri pivot 1. non testé

--- list_tri.ml
+++ list_tri.ml
@@ -2,6 +2,23 @@
 
 (* fonctions de comparaison *)
 
-let croissant x y = (x<= y) ;;
-let décroissant x y = (x>= y) ;;
+let croissant x y = (x <= y) ;;
+let decroissant x y = (x >= y) ;;
+
+(* Tri pivot *)
+
+(* 1. *)
+
+let partitionne_pivot_bis croissant l pivot l1 l2 =
+match l with
+[]-> (l1, l2)
+|x::r -> if (croissant x pivot)
+         then (x::l1,l2)
+         else (l1, x::l2) ;;
+
+let partionne_pivot croissant l pivot =
+partitionne_pivot_bis croissant l pivot [] [] ;;
+
+(* 2. *) 
+
 
