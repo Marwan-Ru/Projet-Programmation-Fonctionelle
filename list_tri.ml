775fb536a9776640c4e4e9f55a313d95c3fbb3fd
Doo5942 <doodescamps@gmail.com>
2019-04-24 21:27:04 +0200

    Tri_pivot ok

--- list_tri.ml
+++ list_tri.ml
@@ -1,54 +1,47 @@
 (* module list tri *)
 
-(* fonctions de comparaison *)
-
-let croissant x y = (x < y) ;;
-
-(*
-let decroissant x y = (x >= y) ;;
-*)
-
 (* Tri pivot *)
 
 (* 1. *)
 
-let rec partitionne_pivot_bis croissant l pivot l1 l2 =
+let rec partitionne_pivot_bis comp l pivot l1 l2 =
 match l with
-[]-> (l1,(pivot::l2))
-|x::r -> if (croissant x pivot)
-            then partitionne_pivot_bis croissant r pivot (x::l1) l2
-            else partitionne_pivot_bis croissant r pivot l1 (x::l2) 
+[] -> (l1,l2)
+|x::r -> if (comp x pivot)
+         then partitionne_pivot_bis comp r pivot (x::l1) l2
+         else partitionne_pivot_bis comp r pivot l1 (x::l2) 
 
-let partitionne_pivot croissant l  =
+let partitionne_pivot comp l pivot =
 match l with 
 [] -> ([],[]) 
-|x::r -> partitionne_pivot_bis croissant r x [] [] ;;
+|x::r -> partitionne_pivot_bis comp l pivot [] [] ;;
 
 (*
  let list = random_list 100 20 ;;
  *)
 
-partitionne_pivot croissant [4;2;6;1;3;9;5] ;;
+partitionne_pivot (>) [4;2;6;1;1;3;9;5] 6 ;;
 
 
 (* 2. *)
 
-
-let rec tri_pivot croissant l =
+let rec tri_pivot comp l =
 if l = []
 then []
-else let (l1,l2) = partitionne_pivot croissant(List.tl l) in
-if (List.length l1) = 1 && (List.length l2) = 1
-then  if (croissant (List.hd l) (List.hd l1))
-      then (List.hd l::[])@ l1 @ l2
-      else if (croissant (List.hd l) (List.hd l2))
-           then l1 @ (List.hd l::[])@ l2
-           else l1 @ l2 @ (List.hd l :: [])
-else if (croissant (List.hd l) (List.hd (List.tl l)))
-     then (tri_pivot croissant l1) @ (List.hd l::[]) @  (tri_pivot croissant l2)
-     else (tri_pivot croissant l1) @ (tri_pivot croissant l2 @ (List.hd l::[])) ;;
+else let pivot = (List.hd l) in
+     let (l1,l2) = partitionne_pivot comp (List.tl l) pivot in
+     if l1 =[] && l2 =[]
+     then pivot::[]
+     else if l1 = [] && l2 <> [] 
+          then ((List.hd l) :: []) @ (tri_pivot comp l2)
+          else if l1<>[] && l2 = []
+               then (tri_pivot comp l1) @ ((List.hd l) :: [])
+               else (tri_pivot comp l1) @ ((List.hd l) :: [])@(tri_pivot comp l2) ;;
 
-tri_pivot croissant [4;2;6;1;3;9;5] ;;	      
+let list = random_list 50 20 ;;
+
+tri_pivot (>) list ;;
+
 
 
 
